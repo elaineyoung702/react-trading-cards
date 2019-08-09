e8758bf168bee938135c73db3e5010be01effbdb
Hackbright Student <no-reply@hackbrightacademy.com>
2019-08-08 18:01:34 -0700

    added link to about page onto homepage

--- static/js/homepage.jsx
+++ static/js/homepage.jsx
@@ -3,7 +3,10 @@ class Homepage extends React.Component {
 		return <div>Welcome! Check out our <a href="/cards">trading cards</a>.
         <br></br><br></br><br></br>
         <img src="https://www.raccoongames.es/med/img/productos/figura-team-fortress-2-balloonicorn-15cm/1380_team_fortress_balloonicorn_vinyl_figure_inhand.jpg"></img>
-        </div>;
+        <br></br>
+        <a href="/about">About the author</a>
+        </div>
+        ;
 	}
 }
 
